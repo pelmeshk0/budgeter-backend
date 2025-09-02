cbdc9f3bd7bb85444ac4ce9b20ae6f48bce4f9ed
Max <maxradomskyy@gmail.com>
2025-09-03 00:44:18 +0200

    fix tests, remove junk files

--- src/test/java/com/radomskyi/budgeter/BudgeterApplicationTests.java
+++ src/test/java/com/radomskyi/budgeter/BudgeterApplicationTests.java
@@ -2,8 +2,10 @@ package com.radomskyi.budgeter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BudgeterApplicationTests {
 
 	@Test
