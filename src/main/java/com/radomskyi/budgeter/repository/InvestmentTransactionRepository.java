c5621ab55434f8840bbfd79faa902717dcb54302
Max <maxradomskyy@gmail.com>
2025-10-15 21:14:42 +0200

    docs: remove resolved comment

--- src/main/java/com/radomskyi/budgeter/repository/InvestmentTransactionRepository.java
+++ src/main/java/com/radomskyi/budgeter/repository/InvestmentTransactionRepository.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-// todo rename this to InvestmentRepository?
 @Repository
 public interface InvestmentTransactionRepository extends JpaRepository<InvestmentTransaction, Long> {
 
